92e3abae73a83305b3ab398d9a7dbf25bed5a116
Vikas Chaurasiya <vikaschaurasiya@vikasc-mbp.local>
2019-01-01 00:03:08 +0530

    added online movie ticket booking system DB project

---
+++ movie_ticket/config.php
@@ -0,0 +1,8 @@
+<?php
+    $host = "127.0.0.1";
+    $user = "root";                     //Your Cloud 9 username
+    $pass = "";                                  //Remember, there is NO password by default!
+    $db = "movies_db";                                  //Your database name you want to connect to
+    $port = 3306;
+     $con = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
+?>
